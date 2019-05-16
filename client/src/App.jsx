4a3a7c465e726b76fb290a7fcbb3f2247afd8369
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-15 18:27:45 -0700

    Implement build environment, server, and react rendering

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>Testing React Functionality</div>
+    );
+  }
+}
+
+export default App;
