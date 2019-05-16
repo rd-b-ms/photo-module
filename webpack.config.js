e428f9a5e9f9dbe2ead861a74984d75b7ffff999
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-15 18:23:48 -0700

    Additional configuration of webpack/package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/public');
+
 module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR,
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader"]',
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/react', '@babel/env'],
         },
       },
     ],
