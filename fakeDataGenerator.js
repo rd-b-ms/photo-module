d46cb5b7662a46dbfe6a2f8eabde9ecb7b9118c2
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-05-17 10:53:29 -0700

    Modify fake data generator to handle images hosted on AWS

--- fakeDataGenerator.js
+++ fakeDataGenerator.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const createFakeData = () => {
   const fakeDataArray = [];
 
-  for (let i = 1; i < 1000; i += 1) {
+  for (let i = 1; i < 697; i += 1) {
     fakeDataArray.push([
-      faker.image.city(),
+      `photos/photo-${i}.jpg`,
       faker.lorem.sentence(),
       faker.random.boolean(),
       faker.random.number({ min: 1, max: 101, precision: 3 }),
