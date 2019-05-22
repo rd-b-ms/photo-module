989648a18594d56ced70406f90ec2cc0c319ed4b
Scott Torres <scotttorres@Scotts-Air.attlocal.net>
2019-05-22 12:21:35 -0700

    Move test files to spec folder

--- spec/Enzyme.test.js
+++ spec/Enzyme.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, mount, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import App from '../client/src/App';
 
 configure({ adapter: new Adapter() });
 
