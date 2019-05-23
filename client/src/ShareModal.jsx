1e4f5a3254edb5e41c1fe63acea080f353c124af
Scott Torres <scotttorres@Scotts-Air.attlocal.net>
2019-05-22 18:29:45 -0700

    Add styled description and inner sections

--- client/src/ShareModal.jsx
+++ client/src/ShareModal.jsx
@@ -21,7 +21,7 @@ const StyledDiv = styled.div`
   font-family: Roboto, Helvetica, sans-serif;
   color: #484848;
   font-size: 14px;
-  width: 25%;
+  width: 23%;
   height: auto;
   top:50%;
   left:50%;
@@ -43,6 +43,7 @@ const StyledCloseButton = styled.button`
 
 const StyledHeader = styled.div`
   display: block;
+  padding: 0px 0px 24px 0px;
 `;
 
 const StyledTitle = styled.h1`
@@ -55,13 +56,52 @@ const StyledTitle = styled.h1`
   color: #484848;
 `;
 
+const StyledListingDescription = styled.div`
+  display: block;
+  word-wrap: break-word;
+  font-size: 14px;
+  font-weight: 300;
+  line-height: 1.28em;
+`;
+
+const StyledInnerSection = styled.section`
+  display: block;
+  padding: 24px 0px;
+  border-bottom: 1px solid rgb(235, 235, 235);
+  border-top: 1px solid rgb(235, 235, 235);
+  font-weight: 400;
+  font-size: 16px;
+  color: #008489;
+`;
+
 const ShareModal = ({ shareModalIsVisible }) => (
   <StyledSection shareModalIsVisible={shareModalIsVisible}>
     <StyledDiv>
       <StyledCloseButton>X</StyledCloseButton>
       <StyledHeader>
         <StyledTitle>Share</StyledTitle>
+        <StyledListingDescription>
+          Check out this awesome listing on Carebnb: (Add fake data here)
+        </StyledListingDescription>
       </StyledHeader>
+      <StyledInnerSection>
+        <span>Facebook</span>
+      </StyledInnerSection>
+      <StyledInnerSection>
+        <span>Twitter</span>
+      </StyledInnerSection>
+      <StyledInnerSection>
+        <span>Email</span>
+      </StyledInnerSection>
+      <StyledInnerSection>
+        <span>Messenger</span>
+      </StyledInnerSection>
+      <StyledInnerSection>
+        <span>Copy Link</span>
+      </StyledInnerSection>
+      <StyledInnerSection>
+        <span>Embed</span>
+      </StyledInnerSection>
     </StyledDiv>
   </StyledSection>
 );
