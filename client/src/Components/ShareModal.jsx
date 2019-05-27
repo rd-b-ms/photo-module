d4cdccc8fdef16e5da8c0f9c7a4a6efbdbc86acd
Scott Torres <scotttorres@Scotts-Air.attlocal.net>
2019-05-27 13:51:48 -0700

    Refactor ShareModal styled components into style.js

--- client/src/Components/ShareModal.jsx
+++ client/src/Components/ShareModal.jsx
@@ -1,86 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-import { FacebookIcon, TwitterIcon, EmailIcon, MessengerIcon, CopyIcon, EmbedIcon, ShareModalCloseIcon } from './svg';
-
-const StyledSection = styled.section`
-  display: ${props => (props.shareModalIsVisible ? 'block' : 'none')};
-  position: fixed;
-  top: 0;
-  left: 0;
-  width:100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.6);
-  z-index: 1;
-`;
-
-const StyledDiv = styled.div`
-  display: block;
-  position:fixed;
-  background: rgb(255, 255, 255);
-  padding: 24px;
-  font-family: Roboto, Helvetica, sans-serif;
-  color: #484848;
-  font-size: 14px;
-  width: 23%;
-  height: auto;
-  top:50%;
-  left:50%;
-  transform: translate(-50%,-50%);
-`;
-
-const StyledCloseButton = styled.button`
-  display: block;
-  padding: 20px;
-  margin: -20px;
-  cursor: pointer;
-  background-color: transparent;
-  font-weight: 100;
-  font-family: inherit;
-  font-size: 24px;
-  border-width: 0px;
-  color: rgb(118, 118, 118);
-`;
-
-const StyledTitle = styled.h1`
-  margin: 15px 0px 0px 0px;
-  padding: 2px 0px;
-  font-weight: 700;
-  font-family: inherit;
-  font-size: 24px;
-  line-height: 1.25em;
-  color: #484848;
-`;
-
-const StyledListingDescription = styled.div`
-  display: block;
-  word-wrap: break-word;
-  font-size: 14px;
-  font-weight: 300;
-  line-height: 1.28em;
-`;
-
-const StyledLinkContainer = styled.section`
-  display: block;
-  vertical-align: top;
-  padding: 24px 0px;
-  border-bottom: 1px solid rgb(235, 235, 235);
-  border-top: 1px solid rgb(235, 235, 235);
-  cursor: pointer;
-`;
-const StyledTextContainer = styled.span`
-  font-weight: 400;
-  font-size: 16px;
-  color: #008489;
-`;
-
-const StyledText = styled.span`
-  ${StyledTextContainer}:hover & {
-    text-decoration: underline;
-  }
-  display: table-cell;
-  vertical-align: top;
-`;
+import {
+  StyledShareModal,
+  ShareModalContainer,
+  StyledCloseButton,
+  ShareModalTitle,
+  StyledListingDescription,
+  StyledLinkContainer,
+  ShareLinkTextContainer,
+  StyledShareLinkText,
+} from '../Styles/style';
+import {
+  FacebookIcon,
+  TwitterIcon,
+  EmailIcon,
+  MessengerIcon,
+  CopyIcon,
+  EmbedIcon,
+  ShareModalCloseIcon,
+} from './svg';
 
 class ShareModal extends React.Component {
   constructor(props) {
@@ -109,17 +47,17 @@ class ShareModal extends React.Component {
     const { shareModalIsVisible } = this.props;
 
     return (
-      <StyledSection
+      <StyledShareModal
         className="share-modal-container"
         onClick={this.handleClickOutsideModal}
         shareModalIsVisible={shareModalIsVisible}
       >
-        <StyledDiv ref={this.ref}>
+        <ShareModalContainer ref={this.ref}>
           <StyledCloseButton className="share-modal-close-button" onClick={this.onCloseButtonClick}>
             <ShareModalCloseIcon height="16px" width="16px" display="block" fill="rgb(118, 118, 118)" />
           </StyledCloseButton>
           <div style={{ display: 'block', padding: '0px 0px 24px 0px' }}>
-            <StyledTitle className="share-modal-title">Share</StyledTitle>
+            <ShareModalTitle className="share-modal-title">Share</ShareModalTitle>
             <StyledListingDescription>
               Check out this awesome listing on Carebnb:
                Veniam voluptatem sit rerum aut sed est in suscipit.
@@ -127,42 +65,42 @@ class ShareModal extends React.Component {
           </div>
           <StyledLinkContainer>
             <FacebookIcon marginRight="8px" height="18px" width="18px" display="inline-block" fill="rgb(72, 72, 72)" />
-            <StyledTextContainer>
-              <StyledText>Facebook</StyledText>
-            </StyledTextContainer>
+            <ShareLinkTextContainer>
+              <StyledShareLinkText>Facebook</StyledShareLinkText>
+            </ShareLinkTextContainer>
           </StyledLinkContainer>
           <StyledLinkContainer>
             <TwitterIcon marginRight="8px" height="18px" width="18px" display="inline-block" fill="rgb(72, 72, 72)" />
-            <StyledTextContainer>
-              <StyledText>Twitter</StyledText>
-            </StyledTextContainer>
+            <ShareLinkTextContainer>
+              <StyledShareLinkText>Twitter</StyledShareLinkText>
+            </ShareLinkTextContainer>
           </StyledLinkContainer>
           <StyledLinkContainer>
             <EmailIcon marginRight="8px" height="18px" width="18px" display="inline-block" fill="rgb(72, 72, 72)" />
-            <StyledTextContainer>
-              <StyledText>Email</StyledText>
-            </StyledTextContainer>
+            <ShareLinkTextContainer>
+              <StyledShareLinkText>Email</StyledShareLinkText>
+            </ShareLinkTextContainer>
           </StyledLinkContainer>
           <StyledLinkContainer>
             <MessengerIcon marginRight="8px" height="18px" width="18px" display="inline-block" fill="rgb(72, 72, 72)" />
-            <StyledTextContainer>
-              <StyledText>Messenger</StyledText>
-            </StyledTextContainer>
+            <ShareLinkTextContainer>
+              <StyledShareLinkText>Messenger</StyledShareLinkText>
+            </ShareLinkTextContainer>
           </StyledLinkContainer>
           <StyledLinkContainer>
             <CopyIcon marginRight="8px" height="18px" width="18px" display="inline-block" fill="rgb(72, 72, 72)" />
-            <StyledTextContainer>
-              <StyledText>Copy Link</StyledText>
-            </StyledTextContainer>
+            <ShareLinkTextContainer>
+              <StyledShareLinkText>Copy Link</StyledShareLinkText>
+            </ShareLinkTextContainer>
           </StyledLinkContainer>
           <StyledLinkContainer>
             <EmbedIcon marginRight="8px" height="18px" width="18px" display="inline-block" fill="rgb(72, 72, 72)" />
-            <StyledTextContainer>
-              <StyledText>Embed</StyledText>
-            </StyledTextContainer>
+            <ShareLinkTextContainer>
+              <StyledShareLinkText>Embed</StyledShareLinkText>
+            </ShareLinkTextContainer>
           </StyledLinkContainer>
-        </StyledDiv>
-      </StyledSection>
+        </ShareModalContainer>
+      </StyledShareModal>
     );
   }
 }
