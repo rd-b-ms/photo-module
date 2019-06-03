0121db32214ff820ac59b8ce460bcee627ed7260
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-03 15:41:44 -0700

    Configure dockerfiles and db scrips for AWS deployment

---
+++ db/fakeDataGenerator.js
@@ -0,0 +1,38 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const createFakeData = () => {
+  const fakeDataArray = [];
+
+  for (let i = 1; i < 697; i += 1) {
+    const j = Math.ceil(Math.random() * 41);
+    console.log(j);
+    fakeDataArray.push([
+      `https://s3-us-west-1.amazonaws.com/fec-carebnb/photos/webP+Photos/photo-${j}.webp`,
+      faker.lorem.sentence(),
+      faker.random.boolean(),
+      Math.ceil((i % 101) + 1),
+    ]);
+  }
+  return fakeDataArray;
+};
+
+const writeDataToFile = (array) => {
+  fs.writeFileSync('fakeData.txt', array[0].toString(), (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+
+  for (let i = 1; i <= array.length - 1; i += 1) {
+    const fakeStr = array[i].toString();
+    fs.appendFile('fakeData.txt', `\n${fakeStr}`, (err) => {
+      if (err) {
+        throw err;
+      }
+    });
+  }
+};
+
+const dataToWrite = createFakeData();
+writeDataToFile(dataToWrite);
---
+++ db/sql-scripts/schema.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS photo_display;
+
+CREATE DATABASE photo_display;
+
+USE photo_display;
+
+CREATE TABLE photo_information (
+  id INT NOT NULL AUTO_INCREMENT,
+  photo_url VARCHAR(255) NOT NULL,
+  description VARCHAR(1000) NOT NULL,
+  is_verified BOOLEAN NOT NULL,
+  listing_id INT,
+  PRIMARY KEY (id)
+);
+
+LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/fakeData.txt'
+INTO TABLE photo_information
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n' (photo_url, description, is_verified, listing_id);
