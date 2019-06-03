9735655e46ca89e0c1d1de595f849f7fd131a87a
Scott Torres <scotttorres@Scotts-MacBook-Air.local>
2019-06-03 15:43:20 -0700

    Complete configuration of files for deployment to AWS

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./../db/index.js');
 
 const app = express();
-const PORT = 3002;
+const PORT = 4000;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
