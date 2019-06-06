f27ff3f34f0d645b1d720c2f254cab4e9fa54d7d
Daniel Kim <nautilus@Nautilus.local>
2019-06-06 14:03:37 -0700

    Add all data gen configs pre-giant generation

--- data_gen/dataGen.js
+++ data_gen/dataGen.js
@@ -1,60 +1,73 @@
 const faker = require('faker');
 const fs = require('fs');
-const csvWriter = require('csv-write-stream');
 
 const startTime = Date.now();
 
-const writer1 = csvWriter();
-const writer2 = csvWriter();
+const writeListings = fs.createWriteStream('./data_gen/fakeDataListings.csv');
+const writePhotos = fs.createWriteStream('./data_gen/fakeDataPhotos.csv');
 
-const count1 = 10000000;
-const count2 = 80000000;
+const listingsCount = 10000;
+const photosCount = 800;
 
 let endTime1;
 let endTime2;
 
-const dataGen1 = (count) => {
-  writer1.pipe(fs.createWriteStream('dataGen1.csv'));
+const dataGenListings = (writer, count) => {
+  let i = count;
+  writer.write('listing\n');
 
-  for (let i = 0; i < count; i += 1) {
-    writer1.write({
-      listing: `${faker.lorem.words()}`,
-    });
-    if (i % 10000 === 0) {
-      console.log(i);
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      const row = [
+        faker.lorem.words(),
+      ];
+      const data = `${row}\n`;
+      if (i === 0) {
+        writer.write(data);
+      } else {
+        ok = writer.write(data);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
     }
   }
-
-  writer1.end();
+  write();
   endTime1 = Date.now();
-  console.log('Data Gen1 successful');
 };
 
-// dataGen1(count1);
+const dataGenPhotos = (writer, count) => {
+  let i = count;
+  writer.write('photo_url,description,is_verified,listing_id\n');
 
-const dataGen2 = (count) => {
-  writer2.pipe(fs.createWriteStream('dataGen2.csv'));
-
-  for (let i = 0; i < count; i += 1) {
-    const photoNumber = Math.floor(Math.random() * 1000);
-    writer2.write({
-      photo_url: `https://sdc-photos-rdbms.s3-us-west-1.amazonaws.com/${photoNumber}.jpeg`,
-      description: faker.lorem.sentence(),
-      is_verified: faker.random.boolean(),
-      lisiting_id: Math.floor(Math.random() * count) + 1,
-    });
-    if (i % 10000 === 0) {
-      console.log(i);
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      const photoNumber = Math.floor(Math.random() * 1000) + 1;
+      const row = [
+        `https://sdc-photos-rdbms.s3-us-west-1.amazonaws.com/${photoNumber}.jpeg,${faker.lorem.sentence()},${faker.random.boolean()},${Math.floor(Math.random() * count) + 1}`,
+      ];
+      const data = `${row}\n`;
+      if (i === 0) { // last time
+        writer.write(data);
+      } else {
+        ok = writer.write(data);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
     }
   }
-
-  writer2.end();
+  write();
   endTime2 = Date.now();
-  console.log('Data Gen2 successful');
 };
 
-dataGen2(count2);
+dataGenListings(writeListings, listingsCount);
+dataGenPhotos(writePhotos, photosCount);
 
-// console.log(`dataGen1 took ${(endTime1 - startTime) / 1000} seconds`);
-// console.log(`dataGen2 took ${(endTime2 - endTime1) / 1000} seconds`);
+console.log(`dataGen1 took ${(endTime1 - startTime) / 1000} seconds`);
+console.log(`dataGen2 took ${(endTime2 - endTime1) / 1000} seconds`);
 console.log(`Total generation time: ${Math.floor((Date.now() - startTime) / 1000)} seconds`);
